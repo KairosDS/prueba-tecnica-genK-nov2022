d52c1eca402e42920b09f1d1eb057e011f5ed66d
Eliana Jordan <76211124+elianajr@users.noreply.github.com>
2022-11-02 18:36:08 +0100

    fix:loop array

--- ejercicio1d.js
+++ ejercicio1d.js
@@ -1,7 +1,7 @@
 const N = 1000;
 const arr = Array.from(Array(N), (_, index) => index + 1);
 
-for (i = 0; i < arr.length; i++) {
+for (let i = 0; i < arr.length; i++) {
     const button = document.createElement("button");
     button.innerHTML = arr[i] + " botón";
     document.body.appendChild(button);
