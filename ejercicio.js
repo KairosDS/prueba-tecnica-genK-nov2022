ba8d3444951b4c46b19f707bb67d11967e54bd08
Folkenciyo <86403857+Folkenciyo@users.noreply.github.com>
2022-11-02 15:26:01 +0100

    feat: new feature changing span when over mouse on each button

--- ejercicio.js
+++ ejercicio.js
@@ -1,4 +1,4 @@
-// Ejercicio 1-d
+// Ejercicio 1-e
 
 
 for (let i = 0; i<1000; i++){
@@ -6,6 +6,8 @@ for (let i = 0; i<1000; i++){
     let span = document.createElement('span');
     let buttonText = `${i+1} botón`
 
+    button.addEventListener('mouseover', (event) => span.innerHTML = "1 botóN");
+    button.addEventListener('mouseout', (event) => span.innerHTML = "1 botón");
     span.innerHTML = buttonText
     button.appendChild(span);
     document.body.append(button);
