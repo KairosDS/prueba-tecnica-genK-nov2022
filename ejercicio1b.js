5722c8a98d010d70ed60c02e103db1995bb1a419
Eliana Jordan <76211124+elianajr@users.noreply.github.com>
2022-11-02 18:32:54 +0100

    fix:button text

--- ejercicio1b.js
+++ ejercicio1b.js
@@ -1,7 +1,7 @@
 let btn = document.querySelector("button");
 
 btn.addEventListener("mouseover", function() {
- 	this.textContent = "1 botón N";
+ 	this.textContent = "1 botóN";
 })
 btn.addEventListener("mouseout", function() {
  	this.textContent = "1 botón";
