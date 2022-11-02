2416de75e51faecef0a0079146bc1c7e308ddeeb
Folkenciyo <86403857+Folkenciyo@users.noreply.github.com>
2022-11-02 21:08:43 +0100

    fix: doing the exercises correctly

--- ejercicio2a.js
+++ ejercicio2a.js
@@ -13,52 +13,28 @@ const array1 = [
   ];
 
 
-// files from array1
-let file1 = array1[0];
-let file2 = array1[1];
-let file3 = array1[2];
+const multArr = (arr1,arr2) =>{
 
-//collums from array2
-let collum1 = [];
-let collum2 = [];
-let collum3 = [];
+    let finalArr = []
 
-// Extraction of the collums from array2
-const collumsArr = (arr) => {
+    for(let i = 0; i < arr1.length; i++){
 
-    for(let i = 0; i < arr.length; i++){
-        collum1.push(arr[i][0]);
-        collum2.push(arr[i][1]);
-        collum3.push(arr[i][2]);
+        
+
+        for(let x = 0; x < arr2.length; x++){
+
+            let multi = 1;
+            multi = multi * arr1[i][x] * arr2[x][i]
+            finalArr.push(multi)
+        }
+        
     }
-};
+    return finalArr;
+}
 
-collumsArr(array2)
+let result = multArr(array1, array2)
 
-// Multiply files from array1 with collums from array2
-const fileColumResult = (file, collum) => {
-    
-    let result = 1
-
-    file.forEach(e => {
-        result = result * e
-    });
-
-    collum.forEach(e => {
-        result = result * e
-    });
-
-    return result;
-};
-
-// Final solution
-let result = [];
-
-// Pushing results to Final solution
-result.push(fileColumResult(file1, collum1));
-result.push(fileColumResult(file2, collum2));
-result.push(fileColumResult(file3, collum3));
-
-// Shows final solution in console
 console.log(result)
+
+
 
