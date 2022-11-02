76079164c13209bc491c69c702849f789ad77100
Manuela Garcia Yebra <manuelagarciayebra@gmail.com>
2022-11-02 17:07:12 +0100

    refactor into a function

--- ejercicio1a.js
+++ ejercicio1a.js
@@ -1,6 +1,10 @@
 'use strict';
 
-const button = document.createElement('button');
-const buttonText = document.createTextNode('1 botón');
-button.appendChild(buttonText);
-document.body.appendChild(button);
+const createDOMbutton = () => {
+  const button = document.createElement('button');
+  const buttonText = document.createTextNode('1 botón');
+  button.appendChild(buttonText);
+  document.body.appendChild(button);
+};
+
+createDOMbutton();
