2a321b75a91eb4f11a6eae82063a68cf72e9f28b
Folkenciyo <86403857+Folkenciyo@users.noreply.github.com>
2022-11-02 15:07:54 +0100

    feat: new feature button

---
+++ ejercicio.js
@@ -0,0 +1,10 @@
+
+
+let button = document.createElement('button');
+let span = document.createElement('span');
+
+
+span.innerHTML = "1 botón"
+button.appendChild(span);
+document.body.append(button);
+
