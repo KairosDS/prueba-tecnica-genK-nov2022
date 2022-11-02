76cc1a86906209ca8904745eb86f811a3d690e26
Folkenciyo <86403857+Folkenciyo@users.noreply.github.com>
2022-11-02 17:00:11 +0100

    refactor: adding the feature to a function

--- ejercicio.js
+++ ejercicio.js
@@ -1,17 +1,23 @@
 // Ejercicio 1-e
 
+const addingButtons = (n) =>{
+    for (let i = 0; i<n; i++){
+        let button = document.createElement('button');
+        let span = document.createElement('span');
+        let buttonText = `${i+1} botón`
 
-for (let i = 0; i<1000; i++){
-    let button = document.createElement('button');
-    let span = document.createElement('span');
-    let buttonText = `${i+1} botón`
-
-    button.addEventListener('mouseover', (event) => span.innerHTML = "1 botóN");
-    button.addEventListener('mouseout', (event) => span.innerHTML = "1 botón");
-    span.innerHTML = buttonText
-    button.appendChild(span);
-    document.body.append(button);
+        button.addEventListener('mouseover', (event) => span.innerHTML = "1 botóN");
+        button.addEventListener('mouseout', (event) => span.innerHTML = "1 botón");
+        span.innerHTML = buttonText
+        button.appendChild(span);
+        document.body.append(button);
+    }
 }
+
+addingButtons(1000);
+
+
+
 
 
 
